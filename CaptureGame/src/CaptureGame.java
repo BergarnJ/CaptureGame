cc74e1d770844bf4c3c7ae48858995579b6276d9
Bergarn <179922662+BergarnJ@users.noreply.github.com>
2024-11-09 13:58:34 +0100

    Add files via upload
    The base

---
+++ CaptureGame/src/CaptureGame.java
@@ -0,0 +1,24 @@
+
+import javax.swing.JFrame;
+
+public class CaptureGame {
+	
+	public static void main(String[] args) throws Exception {
+        
+        JFrame game_Window = new JFrame();
+		game_Window.setSize(1600, 800);
+        game_Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game_Window.setVisible(true);
+
+
+
+        boolean running = true;
+
+        while(running){
+            
+        }
+	}
+
+   
+    
+}
